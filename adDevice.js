00a0d66c5f95bf968de66961ec3fd9e7b699ca06
Oliver Zhang <[oli.zhang@gmail.com]>
2017-09-21 17:54:04 +0800

    udpatre code

--- adDevice.js
+++ adDevice.js
@@ -1,26 +1,30 @@
 var adDevices = [
   {
-    id:"1000",
-    title:"PC",
-    position: adPatternPC,
+    id:'1000',
+    title:'PC',
+    description: '',
+    pattern: adPatternPC,
     channels: adChannelsPC
   },
   {
-    id:"2000",
-    title:"iPhone",
-    position: adPatternPhone,
+    id:'2000',
+    title:'iPhone',
+    description: '',
+    pattern: adPatternPhone,
     channels: adChannelsMobile
   },
   {
-    id:"3000",
-    title:"Android",
-    position: adPatternPhone,
+    id:'3000',
+    title:'Android',
+    description: '',
+    pattern: adPatternPhone,
     channels: adChannelsMobile
   },
   {
-    id:"4000",
-    title:"iPad",
-    position: adPatternPad,
+    id:'4000',
+    title:'iPad',
+    description: '',
+    pattern: adPatternPad,
     channels: adChannelsMobile
   }
 ]
